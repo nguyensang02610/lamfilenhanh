894b77428666927299e3e560b5f82bc5d304b2a4
Sang <nguyensang02610@gmail.com>
2023-04-11 00:13:12 +0700

    upload

---
+++ resources/views/content/laravel-example/user-management.blade.php
@@ -0,0 +1,163 @@
+@extends('layouts/layoutMaster')
+
+@section('title', 'User Management - Crud App')
+
+@section('vendor-style')
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-buttons-bs5/buttons.bootstrap5.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/select2/select2.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/formvalidation/dist/css/formValidation.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/animate-css/animate.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/sweetalert2/sweetalert2.css') }}" />
+@endsection
+
+@section('vendor-script')
+    <script src="{{ asset('assets/vendor/libs/moment/moment.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/select2/select2.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/formvalidation/dist/js/FormValidation.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/formvalidation/dist/js/plugins/Bootstrap5.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/formvalidation/dist/js/plugins/AutoFocus.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/cleavejs/cleave.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/cleavejs/cleave-phone.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/sweetalert2/sweetalert2.js') }}"></script>
+@endsection
+
+@section('page-script')
+    <script src="{{ asset('js/laravel-user-management.js') }}"></script>
+@endsection
+
+@section('content')
+    <div class="row g-4 mb-4">
+        <div class="col-sm-6 col-xl-3">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex align-items-start justify-content-between">
+                        <div class="content-left">
+                            <span>Users</span>
+                            <div class="d-flex align-items-end mt-2">
+                                <h3 class="mb-0 me-2">{{ $totalUser }}</h3>
+                                <small class="text-success">(100%)</small>
+                            </div>
+                            <small>Total Users</small>
+                        </div>
+                        <span class="badge bg-label-primary rounded p-2">
+                            <i class="ti ti-user ti-sm"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-xl-3">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex align-items-start justify-content-between">
+                        <div class="content-left">
+                            <span>Verified Users</span>
+                            <div class="d-flex align-items-end mt-2">
+                                <h3 class="mb-0 me-2">{{ $verified }}</h3>
+                                <small class="text-success">(+95%)</small>
+                            </div>
+                            <small>Recent analytics </small>
+                        </div>
+                        <span class="badge bg-label-success rounded p-2">
+                            <i class="ti ti-user-check ti-sm"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-xl-3">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex align-items-start justify-content-between">
+                        <div class="content-left">
+                            <span>Duplicate Users</span>
+                            <div class="d-flex align-items-end mt-2">
+                                <h3 class="mb-0 me-2">{{ $userDuplicates }}</h3>
+                                <small class="text-success">(0%)</small>
+                            </div>
+                            <small>Recent analytics</small>
+                        </div>
+                        <span class="badge bg-label-danger rounded p-2">
+                            <i class="ti ti-users ti-sm"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-xl-3">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex align-items-start justify-content-between">
+                        <div class="content-left">
+                            <span>Verification Pending</span>
+                            <div class="d-flex align-items-end mt-2">
+                                <h3 class="mb-0 me-2">{{ $notVerified }}</h3>
+                                <small class="text-danger">(+6%)</small>
+                            </div>
+                            <small>Recent analytics</small>
+                        </div>
+                        <span class="badge bg-label-warning rounded p-2">
+                            <i class="ti ti-user-circle ti-sm"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Users List Table -->
+    <div class="card">
+        <div class="card-header">
+            <h5 class="card-title mb-0">Tìm kiếm lọc</h5>
+        </div>
+        <div class="card-datatable table-responsive">
+            <table class="datatables-users table">
+                <thead class="border-top">
+                    <tr>
+                        <th></th>
+                        <th>Id</th>
+                        <th>Tên</th>
+                        <th>Email</th>
+                        <th>Xác thực</th>
+                        {{-- <th>Lượt sử dụng</th> --}}
+                        <th>Hành động</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+        <!-- Offcanvas to add new user -->
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasAddUser" aria-labelledby="offcanvasAddUserLabel">
+            <div class="offcanvas-header">
+                <h5 id="offcanvasAddUserLabel" class="offcanvas-title"></h5>
+                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body mx-0 flex-grow-0">
+                <form class="add-new-user pt-0" id="addNewUserForm">
+                    <input type="hidden" name="id" id="user_id">
+                    <div class="mb-3">
+                        <label class="form-label" for="add-user-fullname">Tên</label>
+                        <input type="text" class="form-control" id="add-user-fullname" name="name"/>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label" for="add-user-email">Email</label>
+                        <input type="text" id="add-user-email" class="form-control" name="email" />
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label" for="add-user-email">Mật khẩu</label>
+                        <input type="password" id="add-user-password" class="form-control" name="password" />
+                    </div>
+                    <div class="mb-3">
+                        <input type="hidden" class="form-control" name="role" value="1"/>
+                    </div>
+                    <div class="mb-3">
+                        <input type="hidden" class="form-control" name="status" value="1"/>
+                    </div>
+                    <button type="submit" class="btn btn-primary me-sm-3 me-1 data-submit">Xác nhận</button>
+                    <button type="reset" class="btn btn-label-secondary" data-bs-dismiss="offcanvas">Thoát</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
